184be9cc4faceedd516589b9f1bdf18d054d9089
Kevin Booms <mrkbooms@gmail.com>
2022-11-02 18:53:56 -0400

    Initial Commit

---
+++ src/main/java/com/kevinbooms/RPGStoreApp.java
@@ -0,0 +1,35 @@
+package com.kevinbooms;
+
+// dependencies added in pom.xml
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/*
+    Data Access and DAO review. An RPGStore Database has been created in
+    PostgreSQL. Connection to this database is established with the Apache
+    BasicDataSource library.
+ */
+public class RPGStoreApp {
+
+    public static void main(String[] args) {
+        RPGStoreApp app = new RPGStoreApp();
+        app.run();
+    }
+
+    private void run() {
+    // Instantiate Datasource and connection String
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/RPGStore");
+    // No credentials are supplied in the Connection String above
+    // Set connection credentials
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres1");
+    // WARNING: Do not store plain text passwords in code
+
+    // With a Database connection established we need to create a JDBC Template
+    // to read the Database (through the datasource)
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+    // JDBC Template opens and closes any required connections whenever a Query or SQL action is performed
+    // so there is no need to concern ourselves with managing connections to the database.
+    }
+}
